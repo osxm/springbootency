7b86fde3860561be94cc3bc6c763f4cef0524543
OSXM <oscar999@mail.ustc.edu.cn>
2023-07-19 23:08:13 +0800

    H2

---
+++ springdata/src/main/java/com/osxm/sb/data/OxSpringDataApplication.java
@@ -0,0 +1,24 @@
+/**
+ @Title: OxSpringDataApplication.java
+* @Package com.osxm.sb.data
+* @Description: TODO
+* @author XM
+* @date 2023年7月19日 下午8:32:33
+* @Copyright: 2023
+* @version V1.0  
+*/
+package com.osxm.sb.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class OxSpringDataApplication {
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxSpringDataApplication.class, args);
+	}
+
+}
---
+++ springdata/src/main/resources/schema-h2.sql
@@ -0,0 +1,5 @@
+CREATE TABLE USR (
+ ID int not null primary key,
+ NAME varchar(100)
+);
+
