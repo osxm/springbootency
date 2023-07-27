4f28b7332c52b4c0722fdf25f895627ce1b9083c
OSXM <oscar999@mail.ustc.edu.cn>
2023-07-27 22:12:38 +0800

    OAuth2

---
+++ springoauth2/src/main/java/com/osxm/sb/security/oauth/OsxmSpringOAuthApplication.java
@@ -0,0 +1,22 @@
+/**
+* @Title: OsxmSpringOAuthApplication.java
+* @Package com.osxm.sb.security.oauth
+* @Description: TODO
+* @author XM
+* @date 2023年7月25日 下午10:22:19
+* @Copyright: 2023
+* @version V1.0  
+*/
+package com.osxm.sb.security.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class OsxmSpringOAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsxmSpringOAuthApplication.class, args);
+	}
+}
---
+++ springweb/src/main/java/com/osxm/sb/web/OsxmSpringWebApplication.java
@@ -0,0 +1,21 @@
+/**
+* @Title: OsxmSpringWebApplication.java
+* @Package com.osxm.sb.web
+* @Description: TODO
+* @author XM
+* @date 2023年7月26日 下午9:11:40
+* @Copyright: 2023
+* @version V1.0  
+*/
+package com.osxm.sb.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsxmSpringWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsxmSpringWebApplication.class, args);
+	}
+}
