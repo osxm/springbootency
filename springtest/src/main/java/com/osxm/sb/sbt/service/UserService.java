e48e92b218956ca7ede90bbe0f19298529abc7ba
Oscar Chen <oscar999@mail.ustc.edu.cn>
2024-10-30 21:34:48 +0800

    Test

--- springtest/src/main/java/com/osxm/sb/sbt/service/UserService.java
+++ springtest/src/main/java/com/osxm/sb/sbt/service/UserService.java
@@ -1,5 +1,9 @@
 package com.osxm.sb.sbt.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.osxm.sb.sbt.model.User;
+import com.osxm.sb.sbt.repository.UserRepository;
 
 public class UserService {
     private final UserRepository userRepository;  
